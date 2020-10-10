4176715f588818e144be6302f97c2e81ee2bc5d8
zwp <zhangwanping@tpson.cn>
2020-10-10 15:16:05 +0800

    Initial commit

---
+++ app/src/main/java/com/example/fragmentbestpractice/News.java
@@ -0,0 +1,25 @@
+package com.example.fragmentbestpractice;
+
+/**
+ * Author: Created by zwp on 2020/10/9.
+ */
+public class News {
+    private String title;
+    private String content;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+}
